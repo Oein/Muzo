418f278a1cc84c82eced39a0014a8c92d4c901bf
Oein <oein0219@gmail.com>
2022-10-16 17:04:48 +0900

    add routes, Logo log

--- src/index.ts
+++ src/index.ts
@@ -21,15 +21,32 @@ import { readFileSync, existsSync, writeFileSync } from "fs";
 import fsExtra from "fs-extra";
 import { stringify as ya_stringify, parse as ya_parse } from "yaml";
 import express from "express";
+import { join as p_join } from "path";
 
 // router imports
-import api_files from "./routes/api/files/list";
+import route_api_files from "./routes/api/files/list";
+import route_root from "./routes/index";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 (async function () {
+  console.log("*     *  *    *  *****   ****");
+  console.log("**   **  *    *     *   *    *");
+  console.log("* * * *  *    *    *    *    *");
+  console.log("*  *  *  *    *   *     *    *");
+  console.log("*     *   ****   *****   ****");
+  console.log("                      by Oein");
   logger.info("Starting muzo...");
+
   fsExtra.ensureDirSync(configDir);
+  fsExtra.ensureDirSync(p_join(configDir, "invites"));
+
   if (!existsSync(configFile)) {
     logger.error("Could not find muzo configuration file");
     logger.info("Creating muzo configuration file...");
@@ -45,7 +62,9 @@ const app = express();
   logger.info(`Reading muzo configuration... (${configFile})`);
   writeFileSync(configFile, ya_stringify(global.config));
 
-  app.use("/api/files", api_files); // dir command api
+  app.use("/api/files", route_api_files); // dir command api
+  app.use("/static", express.static(p_join(__dirname, "/static")));
+  app.use("/", route_root);
 
   app.listen(global.config.port, () => {
     logger.success(
