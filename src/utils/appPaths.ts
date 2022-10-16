9137555d7e805794f67bfdc2e76c286cf51207ed
Oein <oein0219@gmail.com>
2022-10-16 21:45:58 +0900

    salt in config -> salt in salt file

--- src/utils/appPaths.ts
+++ src/utils/appPaths.ts
@@ -4,4 +4,5 @@ import { join } from "path";
 const appPaths = envPaths("Muzo");
 
 export const configFile = join(appPaths.config, "config.yaml");
+export const saltFile = join(appPaths.config, "salt.encrypted");
 export const configDir = appPaths.config;
