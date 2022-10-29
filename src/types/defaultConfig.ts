5249a10302f77baf3a77e42a8f51b986b290a1fc
Oein <oein0219@gmail.com>
2022-10-30 08:35:20 +0900

    change struct

--- src/types/defaultConfig.ts
+++ src/types/defaultConfig.ts
@@ -1,22 +1,23 @@
-import { idToAuthKey } from "../utils/authKey";
+import { textToAuthKey } from "../utils/authKey";
 import { Config } from "./config";
 import { permission } from "./permission";
 
-const defaultConfig: Config = {
-  serverName: "My Muzo Server",
-  port: 3000,
-  users: [
-    {
-      name: "admin",
-      permission: permission.Admin,
-      allowedPaths: [
-        {
-          pathD: "/",
-          permissions: [permission.Read, permission.Write, permission.Delete],
-        },
-      ],
-      authed: idToAuthKey("admin", global.salt),
-    },
-  ],
+export default (): Config => {
+  return {
+    serverName: "My Muzo Server",
+    port: 3000,
+    users: [
+      {
+        name: "admin",
+        permission: permission.Admin,
+        allowedPaths: [
+          {
+            pathD: "/",
+            permissions: [permission.Read, permission.Write, permission.Delete],
+          },
+        ],
+        authed: textToAuthKey("admin", global.salt),
+      },
+    ],
+  };
 };
-export default defaultConfig;
