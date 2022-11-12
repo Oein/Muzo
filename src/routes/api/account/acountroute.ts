a56068c2efba1eb5b7d249680a957bb69c174978
Oein <oein0219@gmail.com>
2022-11-12 21:37:32 +0900

    sign out route

--- src/routes/api/account/acountroute.ts
+++ src/routes/api/account/acountroute.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import signinroute from "./signin/signinroute";
 import sessionroute from "./session/sessionroute";
+import signoutroute from "./signout/signoutroute";
 
 router.use((req, res, next) => {
   next();
@@ -9,5 +10,6 @@ router.use((req, res, next) => {
 
 router.use("/signin", signinroute);
 router.use("/session", sessionroute);
+router.use("/signout", signoutroute);
 
 export default router;
--- src/routes/api/account/signout/signoutroute.ts
+++ src/routes/api/account/signout/signoutroute.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { init as get_db } from "../../../../utils/database";
 import { uid } from "uid";
 import { databasion } from "../session/sessionroute";
+import consoleColor from "../../../../logger/consoleColor.mjs";
 
 const router = express.Router();
 
@@ -11,17 +12,26 @@ router.use((req, res, next) => {
   next();
 });
 
-router.post("/request", (req, res) => {
+router.get("/request", (req, res) => {
   let id = req.query.id;
 
   (async function () {
     let id = uid(128);
-    db.delete(
+    console.log(
+      consoleColor.FgRed + "[DB][DEL]",
+      consoleColor.Reset +
+        `session.${databasion(
+          req.headers["user-agent"] || Math.random().toString(),
+          req.ip
+        )}.${id}`
+    );
+    await db.delete(
       `session.${databasion(
         req.headers["user-agent"] || Math.random().toString(),
         req.ip
       )}.${id}`
     );
+    res.send("Done!");
   })();
 });
 
