138e009acc57f00235125a5212076afbc9caae20
Oein <oein0219@gmail.com>
2022-11-12 21:00:28 +0900

    main router

---
+++ src/routes/main/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { join as p_join } from "path";
+const router = express.Router();
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+router.use((req, res, next) => {
+  next();
+});
+
+router.get("/*", (req, res) => {
+  res.sendFile(p_join(__dirname, "..", "..", "public", "main", "index.html"));
+});
+
+export default router;
