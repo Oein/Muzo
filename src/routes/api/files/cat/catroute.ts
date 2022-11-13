ae6686d95b8d2a3d3c80c4cf3d860c4dd7102d79
Oein <oein0219@gmail.com>
2022-11-13 20:27:59 +0900

    cat api

---
+++ src/routes/api/files/cat/catroute.ts
@@ -0,0 +1,79 @@
+import express from "express";
+import { permission } from "../../../../types/permission";
+import getUserByDBedUserId from "../../../../utils/getUserByUserDBedID";
+import validSession from "../../../../utils/validSession";
+import { join as p_join } from "path";
+
+import { pathExists } from "fs-extra";
+
+const router = express.Router();
+
+// middleware that is specific to this router
+router.use((req, res, next) => {
+  next();
+});
+
+router.get("/", async (req, res) => {
+  let drive = req.query.drive;
+  let path = req.query.path;
+
+  if (drive == undefined || path == undefined) {
+    res.send(
+      JSON.stringify({
+        e: "Querys not found",
+      })
+    );
+    return;
+  }
+
+  let isV = await validSession(req);
+
+  if (isV.valid == false) {
+    res.send(
+      JSON.stringify({
+        e: "Session is not valid",
+      })
+    );
+    return;
+  }
+
+  let user = await getUserByDBedUserId(isV.id);
+  let reqDrives = user.allowedPaths.filter((p) => p.pathD == drive);
+
+  if (reqDrives.length == 0) {
+    res.send(
+      JSON.stringify({
+        e: "Don't have permission to access this drive",
+      })
+    );
+    return;
+  }
+
+  let reqDrive = reqDrives[0];
+  let readPermission = reqDrive.permissions.filter((p) => p == permission.Read);
+
+  if (readPermission.length == 0) {
+    res.send(
+      JSON.stringify({
+        e: "Don't have permission to read this drive",
+      })
+    );
+    return;
+  }
+
+  let joinedP = p_join(drive as string, path as string);
+
+  pathExists(joinedP).then((ex) => {
+    if (!ex) {
+      res.send(
+        JSON.stringify({
+          e: "File does not exist",
+        })
+      );
+      return;
+    }
+    res.sendFile(joinedP);
+  });
+});
+
+export default router;
--- src/routes/api/files/fileroute.ts
+++ src/routes/api/files/fileroute.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import drives_route from "./drives/drivesroute";
 import ls_route from "./ls/lsroute";
+import cat_route from "./cat/catroute";
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
@@ -11,5 +12,6 @@ router.use((req, res, next) => {
 
 router.use("/drives", drives_route);
 router.use("/ls", ls_route);
+router.use("/cat", cat_route);
 
 export default router;
