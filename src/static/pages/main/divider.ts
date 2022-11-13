9d96fa8a9e8d2f3f04f294301d48ef2dd32bcdb2
Oein <oein0219@gmail.com>
2022-11-13 17:00:47 +0900

    tools width

--- src/static/pages/main/divider.ts
+++ src/static/pages/main/divider.ts
@@ -1,6 +1,7 @@
 let drives__ = document.getElementById("sidebar") as HTMLDivElement;
 let divider__ = document.querySelector("#divider") as HTMLDivElement;
 let files__ = document.getElementById("files") as HTMLDivElement;
+let tools__ = document.getElementById("tools") as HTMLDivElement;
 
 let moving = false;
 
@@ -17,7 +18,13 @@ document.addEventListener("mousemove", (e) => {
   let x = e.clientX;
 
   let window_w = window.innerWidth;
+  x = Math.max((window_w / 100) * 12, x);
+  x = Math.min((window_w / 100) * 50, x);
 
   drives__.style.width = `${(x / window_w) * 100}vw`;
+  divider__.style.left = `${(x / window_w) * 100}vw`;
+  files__.style.left = `${((x + 3) / window_w) * 100}vw`;
+  tools__.style.left = `${((x + 3) / window_w) * 100}vw`;
   files__.style.width = `${((window_w - x - 3) / window_w) * 100}vw`;
+  tools__.style.width = `${((window_w - x - 3) / window_w) * 100}vw`;
 });
