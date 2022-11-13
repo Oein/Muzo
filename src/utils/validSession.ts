f5a66519ff55089f30febe4135484ed46fc6805d
Oein <oein0219@gmail.com>
2022-11-13 20:39:33 +0900

    session valid with auth token

--- src/utils/validSession.ts
+++ src/utils/validSession.ts
@@ -32,3 +32,27 @@ export default async function validSession(
     id: dt as string,
   };
 }
+
+export async function validSession_(
+  req: Request,
+  auth: string
+): Promise<validSessionRET> {
+  let dt = await db.get(
+    `session.${databasion(
+      req.headers["user-agent"] || Math.random().toString(),
+      req.ip
+    )}.${auth}`
+  );
+
+  if (dt == null || dt === undefined) {
+    return {
+      valid: false,
+      id: "",
+    };
+  }
+
+  return {
+    valid: true,
+    id: dt as string,
+  };
+}
