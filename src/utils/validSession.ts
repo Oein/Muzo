45d9c0f5109b21e910d4aa97223cacb0fcccdb17
Oein <oein0219@gmail.com>
2022-11-13 13:28:31 +0900

    session valid check util

---
+++ src/utils/validSession.ts
@@ -0,0 +1,34 @@
+import { Request } from "express";
+import { databasion } from "../routes/api/account/session/sessionroute";
+import { init as init_db } from "./database";
+
+let db = init_db();
+
+export interface validSessionRET {
+  valid: boolean;
+  id: string;
+}
+
+export default async function validSession(
+  req: Request
+): Promise<validSessionRET> {
+  let auth = req.headers.authorization;
+  let dt = await db.get(
+    `session.${databasion(
+      req.headers["user-agent"] || Math.random().toString(),
+      req.ip
+    )}.${auth}`
+  );
+
+  if (dt == null || dt === undefined) {
+    return {
+      valid: false,
+      id: "",
+    };
+  }
+
+  return {
+    valid: true,
+    id: dt as string,
+  };
+}
