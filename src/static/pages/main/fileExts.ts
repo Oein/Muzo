691d74db47ec936b9ce83f06f5c1ab735458c8a7
Oein <oein0219@gmail.com>
2022-11-13 19:22:45 +0900

    lots -> one module

---
+++ src/static/pages/main/fileExts.ts
@@ -0,0 +1,11 @@
+import ecode from "./exts/code";
+import eimage from "./exts/image";
+import efont from "./exts/font";
+import evideo from "./exts/video";
+import eaudio from "./exts/audio";
+
+export let codeExts = ecode;
+export let imageExts = eimage;
+export let fontExts = efont;
+export let videoExts = evideo;
+export let audioExts = eaudio;
