a3fa580699d67f9198b5a7a913bac118647f3414
Oein <oein0219@gmail.com>
2022-11-14 17:08:55 +0900

    one file select -> files select

--- src/static/pages/main/fileSystem.ts
+++ src/static/pages/main/fileSystem.ts
@@ -13,6 +13,7 @@ import {
 
 let drives = document.getElementById("drives") as HTMLDivElement;
 let files = document.getElementById("files") as HTMLDivElement;
+let imgs = document.getElementById("imgs") as HTMLDivElement;
 
 session;
 
@@ -20,12 +21,108 @@ let driveSelected = 0;
 let drives_paths: string[] = [];
 let path = "/";
 
-let file_selected = -1;
+let file_selected: number[] = [];
 let file_c = 0;
 
 let show_hidden_files = false;
 
 let last_file_click = new Date().getTime() - 100000;
+
+let os: string = "";
+let ua = navigator.userAgent as string;
+
+// remove zoombox
+let removes = [
+  "zoombox-about",
+  "zoombox-count",
+  "zoombox-play",
+  "zoombox-next",
+  "zoombox-prev",
+  "zoombox-preview",
+];
+removes.map((v) => {
+  (document.getElementsByClassName(v)[0] as any).style.display = "none";
+});
+((window as any).zoombox as any).option({
+  enableZoomButton: true,
+  hideWatermark: true,
+})(
+  // user os
+  function () {
+    if (ua.match(/Win(dows )?NT 6\.0/)) {
+      os = "Windows Vista";
+    } else if (ua.match(/Win(dows )?(NT 5\.1|XP)/)) {
+      os = "Windows XP";
+    } else {
+      if (
+        ua.indexOf("Windows NT 5.1") != -1 ||
+        ua.indexOf("Windows XP") != -1
+      ) {
+        os = "Windows XP";
+      } else if (
+        ua.indexOf("Windows NT 7.0") != -1 ||
+        ua.indexOf("Windows NT 6.1") != -1
+      ) {
+        os = "Windows 7";
+      } else if (
+        ua.indexOf("Windows NT 8.0") != -1 ||
+        ua.indexOf("Windows NT 6.2") != -1
+      ) {
+        os = "Windows 8";
+      } else if (
+        ua.indexOf("Windows NT 8.1") != -1 ||
+        ua.indexOf("Windows NT 6.3") != -1
+      ) {
+        os = "Windows 8.1";
+      } else if (
+        ua.indexOf("Windows NT 10.0") != -1 ||
+        ua.indexOf("Windows NT 6.4") != -1
+      ) {
+        os = "Windows 10";
+      } else if (
+        ua.indexOf("iPad") != -1 ||
+        ua.indexOf("iPhone") != -1 ||
+        ua.indexOf("iPod") != -1
+      ) {
+        os = "Apple iOS";
+      } else if (ua.indexOf("Android") != -1) {
+        os = "Android OS";
+      } else if (ua.match(/Win(dows )?NT( 4\.0)?/)) {
+        os = "Windows NT";
+      } else if (ua.match(/Mac|PPC/)) {
+        os = "Mac OS";
+      } else if (ua.match(/Linux/)) {
+        os = "Linux";
+      } else if (ua.match(/(Free|Net|Open)BSD/)) {
+        os = RegExp.$1 + "BSD";
+      } else if (ua.match(/SunOS/)) {
+        os = "Solaris";
+      }
+    }
+    if (os.indexOf("Windows") != -1) {
+      if (
+        navigator.userAgent.indexOf("WOW64") > -1 ||
+        navigator.userAgent.indexOf("Win64") > -1
+      ) {
+        os += " 64bit";
+      } else {
+        os += " 32bit";
+      }
+    }
+  }
+)();
+
+// blank click
+files.addEventListener("click", () => {
+  if (file_selected.length == 0 && file_c == 0) {
+    file_selected.map((v) => {
+      files.children[v].className = "";
+    });
+
+    file_selected = [];
+  }
+  file_c = 0;
+});
 
 export function changePath(driveX: number, pathX: string) {
   drives.children[driveSelected].className = "";
@@ -35,13 +132,18 @@ export function changePath(driveX: number, pathX: string) {
   lsAndShow();
 }
 
-files.addEventListener("click", () => {
-  if (file_selected != -1 && file_c == 0) {
-    files.children[file_selected].className = "";
-    file_selected = -1;
+function classNamer() {
+  file_selected.sort();
+  if (file_selected[file_selected.length - 1] > files.childElementCount) return;
+  for (let i = 0; i < files.childElementCount; i++) {
+    files.children[i].className = "";
+    (files.children[i].children[0] as HTMLInputElement).checked = false;
   }
-  file_c = 0;
-});
+  file_selected.forEach((v, i) => {
+    files.children[v - 1].className = "driveSelected";
+    (files.children[v - 1].children[0] as HTMLInputElement).checked = true;
+  });
+}
 
 function download(url: string, name: string) {
   let encodedUri = encodeURI(url);
@@ -58,16 +160,104 @@ function download(url: string, name: string) {
   done();
 }
 
-function createFileDiv(name: string, iconText: string = " folder ") {
+function createFileDiv(
+  name: string,
+  iconText: string = " folder ",
+  checkBox: boolean = false
+) {
   let containor = document.createElement("div");
   let icon = document.createElement("span");
   icon.className = "material-symbols-outlined icon";
   icon.innerText = iconText;
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "cb";
   let nameElement = document.createElement("div");
   nameElement.innerText = name;
+  if (checkBox) containor.append(checkbox);
   containor.append(icon);
   containor.append(nameElement);
   return containor;
+}
+
+function fileClickHnadler(
+  e: MouseEvent,
+  i: number,
+  d: { type: string; name: string }
+) {
+  if (
+    (e.target as HTMLInputElement | HTMLDivElement).classList.contains("cb")
+  ) {
+    // input
+    let target = e.target as HTMLInputElement;
+    if (target.checked) {
+      if (!file_selected.includes(i)) file_selected.push(i);
+    } else {
+      if (file_selected.includes(i))
+        file_selected.splice(file_selected.indexOf(i), 1);
+    }
+    classNamer();
+  } else {
+    // div
+    let target = e.target as HTMLDivElement;
+    if (
+      ((os.includes("Mac") || os.includes("Apple")) && e.metaKey) ||
+      ((os.includes("Windows") || os.includes("Linux")) && e.ctrlKey)
+    ) {
+      if (file_selected.includes(i))
+        file_selected.splice(file_selected.indexOf(i), 1);
+      else file_selected.push(i);
+    } else {
+      if (
+        file_selected.length == 1 &&
+        file_selected[0] == i &&
+        new Date().getTime() - last_file_click < 300
+      ) {
+        if (d.type == "dir") {
+          path += d.name + "/";
+          ABPath.forward(driveSelected, path);
+          lsAndShow();
+        }
+        if (d.type.startsWith("slk")) {
+          alert("Muzo doesn't supports Symbolic links.");
+        }
+        if (d.type == "fil") {
+          let dns = d.name.split(".");
+          if (d.name.startsWith(".") == false && dns.length < 2) return;
+          if (d.name.startsWith(".") && dns.length < 3) return;
+          let ext = dns[dns.length - 1].toLocaleLowerCase();
+          if (audioExts.includes(ext)) alert("Play audio");
+          if (videoExts.includes(ext)) alert("Play Movie");
+          if (codeExts.includes(ext)) alert("Editor");
+          if (imageExts.includes(ext)) {
+            let img_ = document.createElement("img");
+            img_.src = encodeURI(
+              `/api/files/cat/?drive=${drives_paths[driveSelected]}&path=${
+                path + d.name
+              }&token=${sessionStorage.getItem("SessionKey")}`
+            );
+            img_.setAttribute("zoombox", "");
+            imgs.innerHTML = "";
+            imgs.append(img_);
+            img_.style.width = "1px";
+            img_.style.height = "1px";
+            img_.style.display = "none";
+            (window as any).zoomBoxExecute();
+            img_.addEventListener("load", () => {
+              img_.click();
+            });
+          }
+          console.log("EXT", ext);
+        }
+
+        last_file_click = new Date().getTime() - 1000;
+        return;
+      }
+      file_selected = [i];
+      last_file_click = new Date().getTime();
+    }
+    classNamer();
+  }
 }
 
 export function lsAndShow() {
@@ -81,10 +271,12 @@ export function lsAndShow() {
     })
     .then((v) => {
       v.data.forEach((d: { type: string; name: string }, i) => {
-        if (d.name.startsWith(".") && !show_hidden_files) return;
+        if (d.name.startsWith(".") && !show_hidden_files) {
+          return;
+        }
         let cfd: HTMLDivElement;
         if (d.type == "dir") {
-          cfd = createFileDiv(d.name);
+          cfd = createFileDiv(d.name, " folder ", true);
         } else if (d.type == "fil") {
           let dns = d.name.split(".");
           let iconName = " draft ";
@@ -108,62 +300,17 @@ export function lsAndShow() {
             if (ext == "txt") iconName = " description ";
             if (ext == "gif") iconName = " gif ";
           }
-          cfd = createFileDiv(d.name, iconName);
+          cfd = createFileDiv(d.name, iconName, true);
         } else if (d.type.startsWith("slk")) {
-          cfd = createFileDiv(d.name, " link ");
+          cfd = createFileDiv(d.name, " link ", true);
         } else {
-          cfd = createFileDiv(d.name, " question_mark ");
+          cfd = createFileDiv(d.name, " question_mark ", true);
         }
 
-        cfd.addEventListener("click", () => {
-          file_c = 1;
-          if (file_selected != -1) {
-            if (file_selected == i) {
-              if (new Date().getTime() - last_file_click < 300) {
-                if (d.type == "dir") {
-                  path += d.name + "/";
-                  ABPath.forward(driveSelected, path);
-                  lsAndShow();
-                  return;
-                } else if (d.type.includes("slk")) {
-                  let t = d.type.replace("slk__", "");
-                  if (t.startsWith("dir")) {
-                    t = t.replace("dir__", "");
-                    path = t;
-                    ABPath.forward(driveSelected, path);
-                    lsAndShow();
-                    return;
-                  } else {
-                    t = t.replace("fil__", "");
-                  }
-                  return;
-                } else if (d.type == "fil") {
-                  load();
-                  download(
-                    encodeURI(
-                      `/api/files/cat/?drive=${
-                        drives_paths[driveSelected]
-                      }&path=${path + d.name}&token=${sessionStorage.getItem(
-                        "SessionKey"
-                      )}`
-                    ),
-                    d.name
-                  );
-                  file_selected = -1;
-                  cfd.className = "";
-                }
-              }
-            }
+        let flc = files.childElementCount;
 
-            if (files.childElementCount > file_selected)
-              files.children[file_selected].className = "";
-            file_selected = -1;
-          }
-          if (file_selected == -1) {
-            file_selected = i;
-            cfd.className = "driveSelected";
-            last_file_click = new Date().getTime();
-          }
+        cfd.addEventListener("click", (e) => {
+          fileClickHnadler(e, flc + 1, d);
         });
 
         files.append(cfd);
