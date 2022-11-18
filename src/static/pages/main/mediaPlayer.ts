f32f1f7a0ef3bcd43396eb315e3ffb28bd67f859
Oein <oein0219@gmail.com>
2022-11-18 17:02:59 +0900

    add classNamer

--- src/static/pages/main/mediaPlayer.ts
+++ src/static/pages/main/mediaPlayer.ts
@@ -1,4 +1,12 @@
 let media = document.getElementById("media") as HTMLSpanElement;
+let audioContainer = document.getElementById(
+  "audio-containor"
+) as HTMLDivElement;
+let videoContainer = document.getElementById(
+  "video-containor"
+) as HTMLDivElement;
+let audioPlayer = document.getElementById("audio-player") as HTMLAudioElement;
+let videoPlayer = document.getElementById("video-player") as HTMLVideoElement;
 
 enum PlayingType {
   Audio,
@@ -11,6 +19,14 @@ let playing: PlayingType = PlayingType.Audio;
 function classNamer() {
   if (playing != PlayingType.NotPlaying) media.classList.add("playing");
   else media.classList.remove("playing");
+
+  videoContainer.className = "";
+  audioContainer.className = "";
+
+  if (playing == PlayingType.Audio) audioContainer.className = "show";
+  else audioPlayer.pause();
+  if (playing == PlayingType.Video) videoContainer.className = "show";
+  else videoPlayer.pause();
 }
 
 classNamer();
