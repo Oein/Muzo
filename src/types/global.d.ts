2a7dc217fed67e5301e9dc4d94539a410e8e2592
Oein <oein0219@gmail.com>
2022-11-19 11:54:43 +0900

    add js metatags

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,10 +1,17 @@
 /* eslint-disable no-var */
 import { Config } from "./config";
 import defaultConfig from "./defaultConfig";
+import a from "jsmediatags";
 
 declare global {
   var config: Config;
   var salt: string;
+
+  interface window {
+    jsmediatags: a;
+  }
 }
+
+window.jsmediatags = a;
 
 globalThis.config = defaultConfig;
