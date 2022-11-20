c8f0c39546460e9b3ab10e97e5cfd31b22886c09
Oein <oein0219@gmail.com>
2022-11-20 19:52:58 +0900

    awsome notifiy type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,16 +2,21 @@
 import { Config } from "./config";
 import defaultConfig from "./defaultConfig";
 import a from "jsmediatags";
+import b from "awesome-notifications";
+
+interface CustomWindow extends Window {
+  jsmediatags: a = a;
+  AWN: b = b;
+}
 
 declare global {
   var config: Config;
   var salt: string;
-
-  interface window {
-    jsmediatags: a;
-  }
 }
 
+declare let window: CustomWindow;
+
 window.jsmediatags = a;
+window.AWN = b;
 
 globalThis.config = defaultConfig;
