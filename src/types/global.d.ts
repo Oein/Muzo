2482848d622100b7a18a7d7c75eab3f6766fc918
Oein <oein0219@gmail.com>
2022-11-20 20:00:49 +0900

    remove window declare

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,22 +1,10 @@
 /* eslint-disable no-var */
 import { Config } from "./config";
 import defaultConfig from "./defaultConfig";
-import a from "jsmediatags";
-import b from "awesome-notifications";
-
-interface CustomWindow extends Window {
-  jsmediatags: a = a;
-  AWN: b = b;
-}
 
 declare global {
   var config: Config;
   var salt: string;
 }
-
-declare let window: CustomWindow;
-
-window.jsmediatags = a;
-window.AWN = b;
 
 globalThis.config = defaultConfig;
