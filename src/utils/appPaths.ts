372a6c9f4fbcd92c7e7cd34b0d60b95b441dfcb1
Oein <oein0219@gmail.com>
2022-11-20 22:26:27 +0900

    ensure dir

--- src/utils/appPaths.ts
+++ src/utils/appPaths.ts
@@ -1,7 +1,11 @@
 import envPaths from "env-paths";
 import { join } from "path";
+import { ensureDirSync } from "fs-extra";
 
 const appPaths = envPaths("Muzo");
+
+ensureDirSync(appPaths.config);
+ensureDirSync(appPaths.data);
 
 export const configFile = join(appPaths.config, "config.yaml");
 export const saltFile = join(appPaths.config, "salt.encrypted");
