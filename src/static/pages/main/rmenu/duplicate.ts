737b73a29653634b594235a57d5d172ab02f18e8
Oein <oein0219@gmail.com>
2022-11-25 21:09:58 +0900

    duplicate right click menu

--- src/static/pages/main/rmenu/duplicate.ts
+++ src/static/pages/main/rmenu/duplicate.ts
@@ -1,8 +1,11 @@
 import {
   file_selected,
+  file_names,
   path,
   driveSelected,
   drives_paths,
+  disSelectAll,
+  lsAndShow,
 } from "../fileSystem.js";
 import { closeRMenu } from "../rclickmenu.js";
 
@@ -15,4 +18,47 @@ export function classNamer() {
 
 rclickDuplicate.addEventListener("click", () => {
   closeRMenu();
+
+  let fn = Object.keys(file_names).map(function (key) {
+    return file_names[key].name;
+  });
+  let fn2 = fn.filter((v, i) => {
+    return file_selected.includes(i);
+  });
+
+  (<any>window).notifier.async(
+    new Promise((resolve, reject) => {
+      axios
+        .get(
+          encodeURI(
+            `/api/files/duplicate?drive=${
+              drives_paths[driveSelected]
+            }&path=${path}&token=${sessionStorage.getItem(
+              "SessionKey"
+            )}&file=${fn2.join("///")}`
+          ),
+          {
+            headers: {
+              Authorization: sessionStorage.getItem("SessionKey"),
+            },
+          }
+        )
+        .then(function (response) {
+          resolve(response.data);
+        })
+        .catch((e) => reject(e));
+    }),
+    (suc) => {
+      (<any>window).notifier.success("Successfully duplicated files");
+      lsAndShow();
+    },
+    (err) => {
+      console.log(err);
+      (<any>window).notifier.warning("Error occurred while duplicating files");
+      lsAndShow();
+    },
+    "Duplicating files..."
+  );
+
+  disSelectAll();
 });
